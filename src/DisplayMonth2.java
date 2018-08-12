aca211db86b5b41810fcc8b8809ff6d0f6e8aca9
mkj4489 <masakijo@gmail.com>
2018-08-12 21:57:55 +0900

    配列の学習

---
+++ src/DisplayMonth2.java
@@ -0,0 +1,8 @@
+public class DisplayMonth2 {
+    public static void main(String[] args) {
+        String[] month = {"January", "February", "March", "April", "May", "June", "August", "September", "October", "November", "December"};
+        for (int i = 0; i < 12; i++) {
+            System.out.println(month[i]);
+        }
+    }
+}
