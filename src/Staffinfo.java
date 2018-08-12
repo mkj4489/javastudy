64cc7e4307ec516271cb0471e5dda1f2c853c200
mkj4489 <masakijo@gmail.com>
2018-08-12 22:07:16 +0900

    コンストラクタの学習

--- src/Staffinfo.java
+++ src/Staffinfo.java
@@ -6,14 +6,22 @@ class Staff {
     String email;
 
     public void sayhello(){
-        System.out.println("Hello" + this.name);
+        System.out.println("Hello " + this.name);
+    }
+
+    public Staff(String name, int staffid, String email) {
+        super();
+        this.name = name;
+        this.staffid = staffid;
+        this.email = email;
     }
 }
 
 public class Staffinfo {
     public static void main(String[] args) {
-        Staff yamada = new Staff();
-        yamada.name = "Taro yamada";
+        Staff yamada = new Staff("Taro Yamada",12345, "yamada@abc.co.jp");
+        //yamada.name = "Taro yamada";
+
 
         yamada.sayhello();
         // System.out.println(yamada.name);
