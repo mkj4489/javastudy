b2c8d3f8a9686bc7b1746dd4ae842d638de25e93
mkj4489 <masakijo@gmail.com>
2018-08-13 21:21:34 +0900

    コンストラクタの学習 getter,setter,constructor

--- src/Staffinfo.java
+++ src/Staffinfo.java
@@ -9,12 +9,70 @@ class Staff {
         System.out.println("Hello " + this.name);
     }
 
+    /*
     public Staff(String name, int staffid, String email) {
         super();
         this.name = name;
         this.staffid = staffid;
         this.email = email;
     }
+    */
+
+    public Staff(String name, int staffid, String email) {
+        this.name = name;
+        this.staffid = staffid;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStaffid() {
+        return staffid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStaffid(int staffid) {
+        this.staffid = staffid;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /*
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStaffid() {
+        return staffid;
+    }
+
+    public void setStaffid(int staffid) {
+        this.staffid = staffid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+*/
 }
 
 public class Staffinfo {
@@ -22,10 +80,12 @@ public class Staffinfo {
         Staff yamada = new Staff("Taro Yamada",12345, "yamada@abc.co.jp");
         //yamada.name = "Taro yamada";
 
-
-        yamada.sayhello();
+        //yamada.sayhello();
         // System.out.println(yamada.name);
-
+        System.out.println("[社員情報]");
+        System.out.println("氏名：" + yamada.getName());
+        System.out.println("社員番号：" + yamada.getStaffid());
+        System.out.println("Email：" + yamada.getEmail());
     }
 
 }
