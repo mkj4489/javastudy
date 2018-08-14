6776dad2f1bf35c8588d27e6bc785a399577f79d
mkj4489 <masakijo@gmail.com>
2018-08-15 00:29:53 +0900

    JavaFXの学習

--- FxApps/src/sample/Main.java
+++ FxApps/src/sample/Main.java
@@ -5,15 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+/*
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
-
+*/
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        /*
         Label label = new Label("Hello JavaFX!");
         TextField field = new TextField();
         Button button = new Button("送信");
@@ -22,7 +25,10 @@ public class Main extends Application {
         pane.setTop(label);
         pane.setCenter(field);
         pane.setBottom(button);
-        Scene scene = new Scene(pane, 400, 300);
+        */
+
+        //Scene scene = new Scene(pane, 400, 300);
+        Scene scene = new Scene(root, 400, 300);
         primaryStage.setScene(scene);
         /*
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
